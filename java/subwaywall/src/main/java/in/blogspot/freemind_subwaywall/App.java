892b935800664bf7c31f0b5eecc769031e456eff
leakycup <soubhik@gmail.com>
2014-12-15 23:42:33 +0530

    skeletal java project: a maven-archetype-quickstart

---
+++ java/subwaywall/src/main/java/in/blogspot/freemind_subwaywall/App.java
@@ -0,0 +1,13 @@
+package in.blogspot.freemind_subwaywall;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
