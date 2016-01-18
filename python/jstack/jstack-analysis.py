544fd73390ab56beaebdbe4f491ca3e8aad314c9
Soubhik <soubhik@gmail.com>
2016-01-19 01:21:24 +0530

    added script for jstack analysis

---
+++ python/jstack/jstack-analysis.py
@@ -0,0 +1,34 @@
+import sys
+import re
+from collections import defaultdict
+
+stack_file = sys.argv[1]
+num_elements_to_print = int(sys.argv[2])
+f = open(stack_file, 'r')
+element_count = defaultdict(int)
+component_count = defaultdict(int)
+
+runnable = False
+prev = None
+for line in f:
+  if not line:
+    runnable = False
+  if 'java.lang.Thread.State: RUNNABLE' in line:
+    runnable = True
+  if runnable:
+    element = line.replace('at ', '')
+    element_count[element] += 1
+    if 'org.apache.solr.handler.component.SearchHandler.handleRequestBody' in element:
+      component_count[prev] += 1
+    #prev = element.replace('[.].*(.*)', '')
+    prev = re.sub('[.][^.(]*[(].*[)]$', '', element)
+
+print '---------- stack element count --------------'
+for element in sorted(element_count, key=element_count.get, reverse=True)[:num_elements_to_print]:
+  print element, element_count[element]
+
+print ''
+print '---------- component count --------------'
+for component in sorted(component_count, key=component_count.get, reverse=True):
+  print component, component_count[component]
+
