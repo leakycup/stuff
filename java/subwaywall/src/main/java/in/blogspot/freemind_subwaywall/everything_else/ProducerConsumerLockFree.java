4b698cbd7c49ea6d591afefafe1fb7a5913d7892
soubhik <soubhik@gmail.com>
2016-08-16 00:49:48 +0530

    lock free producer consumer: version two. testing in progress.

--- java/subwaywall/src/main/java/in/blogspot/freemind_subwaywall/everything_else/ProducerConsumerLockFree.java
+++ java/subwaywall/src/main/java/in/blogspot/freemind_subwaywall/everything_else/ProducerConsumerLockFree.java
@@ -1,45 +1,16 @@
 package in.blogspot.freemind_subwaywall.everything_else;
 
-import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.atomic.AtomicLong;
-import java.util.concurrent.atomic.AtomicReference;
+import java.util.*;
+import java.util.concurrent.atomic.*;
 
 /**
  * Created by soubhik on 14-08-2016.
  */
 public class ProducerConsumerLockFree {
     public static class LockFreeCircularFifo<T> {
-        // thread unsafe BufferedItem implementation. at most one thread can have access to a BufferedItem.
-        // use of the atomic variables in this implementation is to implement a memory barrier:
-        // after a thread has updated a buffered item, a different thread should be able to see the updated value.
-        private static class BufferedItem <T> {
-            final AtomicReference<T> item;
-            final AtomicBoolean isEmpty;
-            final AtomicLong accessCount; //TODO: sbh: is this needed? why?
-
-            BufferedItem() {
-                item = new AtomicReference<>();
-                isEmpty = new AtomicBoolean(true);
-                accessCount = new AtomicLong(0);
-            }
-
-            void add(T item) {
-                this.item.set(item);
-                accessCount.getAndIncrement();
-                isEmpty.set(false);
-            }
-
-            T remove() {
-                if (isEmpty.compareAndSet(false, true) == false) {
-                    return null;
-                }
-                accessCount.getAndIncrement();
-                return item.get();
-            }
-        }
-
-        private final AtomicReference<T>[] buffer;
+        private final AtomicReferenceArray<T> buffer;
+        private final AtomicReferenceArray<Boolean> watches;
+        private final int size;
         // atomic reference to immutable objects seems to be the only sane way to use the Atomic* types in Java.
         // since the object being referred to is immutable, a reference check is sufficient to determine
         // if it has changed. ABA problem can still happen if two different objects end up with same reference (at
@@ -50,53 +21,249 @@ public class ProducerConsumerLockFree {
         public LockFreeCircularFifo(int size) {
             assert (size > 0);
 
-            this.buffer = new AtomicReference[size];
+            this.buffer = new AtomicReferenceArray<T>(size);
+            this.watches = new AtomicReferenceArray<Boolean>(size);
+            for (int i = 0; i < size; i++) {
+                watches.set(i, Boolean.FALSE);
+            }
+            this.size = size;
             this.head = new AtomicReference<>(new Integer(0));
             this.tail = new AtomicReference<>(new Integer(0)); //TODO: sbh: make it AtomicInteger: no CAS on tail, hence no ABA problem
         }
 
-        public boolean insert(T item) {
-            int currentTail = tail.get();
-            int nextTail = (currentTail + 1) % buffer.length;
-            // if the following CAS fails, it does not necessarily mean the buffer is full.
-            // it's possible that another thread has won the race and inserted an item in the slot buffer[currentTail].
-            if (buffer[currentTail].compareAndSet(null, item)) {
-                // 1. if a producer thread preempts the current thread at this point, it's going to find that
-                // buffer[currentTail] is not empty and the above CAS will fail for it, even though
-                // the buffer is not full. we can fixing this and simplify the body of this function by ensuring that
-                // at most one thread can be executing the body of this function. this can be achieved by spinlocking
-                // on an atomic boolean at the beginning of this function. this, does not prevent concurrent
-                // execution of the insert() and remove() though.
-                // 2. if a consumer thread preempts the current thread at this point, and happens to consume the item
-                // from buffer[currentTail], then gets preempted by a different producer thread, the producer will
-                // find that the buffer[currentTail] is empty, will insert an item at this position. it can then
-                // insert more items and keep advancing the tail. eventually, when the control returns to the current
-                // thread, it will set the tail to a stale value, an index where an item has already been inserted by
-                // the 2nd producer. doing a CAS rather than set() will avoid this problem.
-                // see http://codereview.stackexchange.com/a/138746/115067
-                tail.set(nextTail);
-                return true;
+        public void insert(T item) throws InterruptedException {
+            assert (item != null);
+
+            Integer currentTail = tail.get();
+            int nextTail = (currentTail + 1) % size;
+            while (!tail.compareAndSet(currentTail, nextTail)) {
+                currentTail = tail.get();
+                nextTail = (currentTail + 1) % size;
             }
 
-            return false;
-        }
-
-        public T remove() {
-            int currentHead = head.get();
-            int nextHead = (currentHead + 1) % buffer.length;
-            T item = buffer[currentHead].get();
-            if (buffer[currentHead].compareAndSet(item, null)) {
-                if (item != null) {
-                    head.set(nextHead);
-                    return item;
+            if (watches.compareAndSet(currentTail, Boolean.FALSE, Boolean.TRUE)) {
+                if (buffer.compareAndSet(currentTail, null, item)) {
+                    watches.set(currentTail, Boolean.FALSE);
+                } else {
+                    Boolean watch = watches.get(currentTail);
+                    synchronized (watch) {
+                        while (!buffer.compareAndSet(currentTail, null, item)) {
+                            watch.wait();
+                        }
+                    }
+                    watches.set(currentTail, Boolean.FALSE);
+                }
+            } else {
+                while (!buffer.compareAndSet(currentTail, null, item));
+                Boolean watch = watches.get(currentTail);
+                synchronized (watch) {
+                    watch.notify();
                 }
             }
+        }
 
-            return null;
+        public T remove() throws InterruptedException {
+            Integer currentHead = head.get();
+            int nextHead = (currentHead + 1) % size;
+            while (!head.compareAndSet(currentHead, nextHead)) {
+                currentHead = head.get();
+                nextHead = (currentHead + 1) % size;
+            }
+
+            if (watches.compareAndSet(currentHead, Boolean.FALSE, Boolean.TRUE)) {
+                T item = buffer.get(currentHead);
+                if (item != null) {
+                    buffer.set(currentHead, null);
+                    watches.set(currentHead, Boolean.FALSE);
+                    return item;
+                } else {
+                    Boolean watch = watches.get(currentHead);
+                    synchronized (watch) {
+                        while ((item = buffer.get(currentHead)) == null) {
+                            watch.wait();
+                        }
+                    }
+                    watches.set(currentHead, Boolean.FALSE);
+                    return item;
+                }
+            } else {
+                T item;
+                while ((item = buffer.get(currentHead)) == null);
+                Boolean watch = watches.get(currentHead);
+                synchronized (watch) {
+                    watch.notify();
+                }
+
+                return item;
+            }
         }
     }
 
+    private static class IntegerProducer extends Thread {
+        private final int start;
+        private final int end;
+        private final int maxSleepTime;
+        private final Random random;
+        private final LockFreeCircularFifo<Integer> fifo;
+        private AtomicBoolean stopNow;
+        private int next;
+
+        IntegerProducer(int start, int end, int maxSleepTime, LockFreeCircularFifo<Integer> fifo) {
+            this.start = start;
+            this.end = end;
+            this.next = start;
+            this.maxSleepTime = maxSleepTime;
+            this.random = new Random();
+            this.fifo = fifo;
+            this.stopNow = new AtomicBoolean(false);
+        }
+
+        @Override
+        public void run() {
+            while (!stopNow.get()) {
+                if (maxSleepTime > 0) {
+                    long sleepTime = random.nextInt(maxSleepTime);
+                    try {
+                        sleep(sleepTime);
+                    } catch (InterruptedException e) {
+                        break;
+                    }
+                }
+
+                try {
+                    fifo.insert(next);
+                } catch (InterruptedException e) {
+                    break;
+                }
+                next++;
+                if (next >= end) {
+                    break;
+                }
+            }
+            System.out.println("produced integers from " + start + " to " + (next-1));
+        }
+
+        public void lazyStop() {
+            this.stopNow.set(true);
+        }
+    }
+
+    private static class IntegerConsumer extends Thread implements Iterable<Integer> {
+        private final int maxSleepTime;
+        private final Random random;
+        private final LockFreeCircularFifo<Integer> fifo;
+        private final List<Integer> numbers;
+        private AtomicBoolean stopNow;
+
+        IntegerConsumer(int maxSleepTime, LockFreeCircularFifo<Integer> fifo) {
+            this.maxSleepTime = maxSleepTime;
+            this.random = new Random();
+            this.fifo = fifo;
+            this.numbers = new ArrayList<>();
+            this.stopNow = new AtomicBoolean(false);
+        }
+
+        @Override
+        public void run() {
+            while (!stopNow.get()) {
+                if (maxSleepTime > 0) {
+                    long sleepTime = random.nextInt(maxSleepTime);
+                    try {
+                        sleep(sleepTime);
+                    } catch (InterruptedException e) {
+                        break;
+                    }
+                }
+
+                int n;
+                try {
+                    n = fifo.remove();
+                } catch (InterruptedException e) {
+                    break;
+                }
+                numbers.add(n);
+
+                System.out.println("consumed n: " + n);
+            }
+        }
+
+        public void lazyStop() {
+            this.stopNow.set(true);
+        }
+
+        @Override
+        public Iterator<Integer> iterator() {
+            return numbers.iterator();
+        }
+    }
+
+    private static void testLockFreeCircularFifo() {
+        System.out.println("Test lock free circular fifo");
+        System.out.println("====================================");
+
+        LockFreeCircularFifo<Integer> fifo = new LockFreeCircularFifo<>(5);
+
+        try {
+            fifo.insert(0);
+            int zero = fifo.remove();
+            assert (zero == 0);
+
+            fifo.insert(1);
+            fifo.insert(2);
+            fifo.insert(3);
+            int one = fifo.remove();
+            int two = fifo.remove();
+            int three = fifo.remove();
+            assert (one == 1);
+            assert (two == 2);
+            assert (three == 3);
+
+            fifo.insert(1);
+            fifo.insert(2);
+            one = fifo.remove();
+            fifo.insert(3);
+            fifo.insert(4);
+            two = fifo.remove();
+            three = fifo.remove();
+            int four = fifo.remove();
+            assert (one == 1);
+            assert (two == 2);
+            assert (three == 3);
+            assert (four == 4);
+
+            fifo.insert(2);
+            fifo.insert(2);
+            int two_1 = fifo.remove();
+            int two_2 = fifo.remove();
+            assert (two_1 == 2);
+            assert (two_2 == 2);
+        } catch (InterruptedException e) {
+            System.out.println("Test lock free circular fifo (single threaded) ended abruptly!!");
+        }
+
+        IntegerProducer producer = new IntegerProducer(2, 7, 10, fifo);
+        IntegerConsumer consumer = new IntegerConsumer(0, fifo);
+        consumer.start();
+        producer.start();
+        try {
+            producer.join();
+            Thread.sleep(20);
+        } catch (InterruptedException e) {
+            System.out.println("Test lock free circular fifo (multi-threaded) ended abruptly!!");
+        }
+        consumer.interrupt();
+        Iterator<Integer> results = consumer.iterator();
+        for (int i = 2; i < 7; i++) {
+            int n = results.next();
+            assert (i == n);
+        }
+        assert (results.hasNext() == false);
+    }
+
     private static void testAtomicReferenceToMutableObject() {
+        System.out.println("Test atomic reference to mutable object");
+        System.out.println("====================================");
+
         StringBuilder b1 = new StringBuilder("builder1");
         StringBuilder b2 = new StringBuilder("builder2");
 
@@ -129,6 +296,9 @@ public class ProducerConsumerLockFree {
     }
 
     private static void testAtomicBasicType() {
+        System.out.println("Test atomic basic type");
+        System.out.println("====================================");
+
         AtomicInteger atomicInteger = new AtomicInteger(5);
 
         //CAS1: 5 --> 6
@@ -165,5 +335,6 @@ public class ProducerConsumerLockFree {
     public static void main(String[] args) {
         testAtomicReferenceToMutableObject();
         testAtomicBasicType();
+        testLockFreeCircularFifo();
     }
 }
