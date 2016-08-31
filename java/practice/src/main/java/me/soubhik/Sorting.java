52a4170604921e0e4d36b6952ad289e6b0b241d7
soubhik <soubhik@gmail.com>
2016-08-31 12:43:26 +0530

    Sorting.java: added automated validation during test, added more tests, fixed bug in partitioning code.

--- java/practice/src/main/java/me/soubhik/Sorting.java
+++ java/practice/src/main/java/me/soubhik/Sorting.java
@@ -27,11 +27,69 @@ public class Sorting {
     }
 
     public static <T extends Comparable<T>> Pair<Integer> dutchNationalFlagPartition(T[] elements, T pivot) {
-       return dutchNationalFlagPartition(elements, pivot, 0, elements.length);
+       return dutchNationalFlagPartition_v2(elements, pivot, 0, elements.length);
     }
 
-    public static <T extends Comparable<T>> Pair<Integer> dutchNationalFlagPartition(T[] elements, T pivot,
-                                                                                     int low, int high) {
+    /**
+     * returns a pair of integers representing the lower (inclusive) and upper (exclusive) indexes
+     * of the pivot elements in the given array.
+     * @param elements input array of elements where we want to determine the final location of the pivot element
+     * @param pivot the element whose final location we seek to obtain in the elements array. the array may contain
+     *              0 or more pivot elements.
+     * @param low lower index of the array (inclusive)
+     * @param high upper index of the array (exclusive)
+     * @param <T> type of the elements. should be a Comparable.
+     * @return pair of integers (x, y) representing, respectively, the lower (inclusive) and the upper (exclusive)
+     * indexes of the pivot elements positions.
+     * x == high iff all elements are lower than pivot.
+     * y == low iff all elements are higher than pivot.
+     * x == y iff pivot is not found between low and high.
+     * otherwise, x >= low, x < high, x < y, y <= high.
+     */
+    public static <T extends Comparable<T>> Pair<Integer> dutchNationalFlagPartition_v2(T[] elements, T pivot,
+                                                                                        int low, int high) {
+        assert (elements != null);
+        assert (low < high);
+
+        int l = low;
+        while (l < high) {
+            if (elements[l].compareTo(pivot) >= 0) {
+                break;
+            }
+            l++;
+        }
+        if (l == high) {
+            //all elements are less than pivot
+            return new Pair<Integer>(high, high);
+        }
+
+        int h = high - 1;
+        while (h >= l) {
+            if (elements[h].compareTo(pivot) <= 0) {
+                break;
+            }
+            h--;
+        }
+
+        int p = l;
+        while (p <= h) {
+            if (elements[p].compareTo(pivot) == 0) {
+                p++;
+            } else if (elements[p].compareTo(pivot) < 0) {
+                swap(elements, l, p);
+                l++;
+                p++;
+            } else {
+                swap(elements, p, h);
+                h--;
+            }
+        }
+
+        return new Pair<>(l, (h+1));
+    }
+
+    public static <T extends Comparable<T>> Pair<Integer> dutchNationalFlagPartition_v1(T[] elements, T pivot,
+                                                                                        int low, int high) {
         assert (elements != null);
         assert (low < high);
 
@@ -88,13 +146,45 @@ public class Sorting {
 
         int pivotIndex = randomInt(low, high);
         T pivot = elements[pivotIndex];
-        Pair<Integer> partitionIndex = dutchNationalFlagPartition(elements, pivot, low, high);
-        quickSort(elements, low, partitionIndex.first); //bug: partitionIndex.first is inclusive
-        quickSort(elements, partitionIndex.second, high); //bug: partitionIndex.second is exclusive
+        Pair<Integer> partitionIndex = dutchNationalFlagPartition_v2(elements, pivot, low, high);
+        quickSort(elements, low, partitionIndex.first);
+        quickSort(elements, partitionIndex.second, high);
     }
 
     public static void radixSort() {
         //TODO
+    }
+
+    private static <T extends Comparable<T>> void validatePartition(T[] elements, int start, int end, T pivot) {
+        validatePartition(elements, 0, elements.length, start, end, pivot);
+    }
+
+    private static <T extends Comparable<T>> void validatePartition(T[] elements, int low, int high,
+                                                                    int start, int end, T pivot) {
+        for (int i = low; i < start; i++) {
+            T e = elements[i];
+            assert (e.compareTo(pivot) < 0);
+        }
+
+        for (int i = start; i < end; i++) {
+            T e = elements[i];
+            assert (e.compareTo(pivot) == 0);
+        }
+
+        for (int i = end; i < high; i++) {
+            T e = elements[i];
+            assert (e.compareTo(pivot) > 0);
+        }
+    }
+
+    private static <T extends Comparable<T>> void validateSort(T[] elements) {
+        assert (elements.length > 0);
+
+        T previous = elements[0];
+        for (T e: elements) {
+            assert (e.compareTo(previous) >= 0);
+            previous = e;
+        }
     }
 
     public static void main(String[] args) {
@@ -113,6 +203,7 @@ public class Sorting {
         }
         System.out.print("\n");
         System.out.println("[" + indices.first + "," + indices.second + ")");
+        validatePartition(inputs1, indices.first, indices.second, 3);
 
         System.out.println("Dutch national flag partition with pivot: " + 3);
         for (int i: inputs1) {
@@ -125,6 +216,7 @@ public class Sorting {
         }
         System.out.print("\n");
         System.out.println("[" + indices.first + "," + indices.second + ")");
+        validatePartition(inputs1, indices.first, indices.second, 3);
 
         System.out.println("Dutch national flag partition with pivot: " + 6);
         for (int i: inputs1) {
@@ -137,6 +229,33 @@ public class Sorting {
         }
         System.out.print("\n");
         System.out.println("[" + indices.first + "," + indices.second + ")");
+        validatePartition(inputs1, indices.first, indices.second, 6);
+
+        System.out.println("Dutch national flag partition with pivot: " + 2);
+        for (int i: inputs1) {
+            System.out.print(i + " ");
+        }
+        System.out.print("\n");
+        indices = dutchNationalFlagPartition(inputs1, 2);
+        for (int i: inputs1) {
+            System.out.print(i + " ");
+        }
+        System.out.print("\n");
+        System.out.println("[" + indices.first + "," + indices.second + ")");
+        validatePartition(inputs1, indices.first, indices.second, 2);
+
+        System.out.println("Dutch national flag partition with pivot: " + 8);
+        for (int i: inputs1) {
+            System.out.print(i + " ");
+        }
+        System.out.print("\n");
+        indices = dutchNationalFlagPartition(inputs1, 8);
+        for (int i: inputs1) {
+            System.out.print(i + " ");
+        }
+        System.out.print("\n");
+        System.out.println("[" + indices.first + "," + indices.second + ")");
+        validatePartition(inputs1, indices.first, indices.second, 8);
 
         System.out.println("Dutch national flag partition with pivot: " + 20);
         for (int i: inputs1) {
@@ -149,6 +268,8 @@ public class Sorting {
         }
         System.out.print("\n");
         System.out.println("[" + indices.first + "," + indices.second + ")");
+        validatePartition(inputs1, indices.first, indices.second, 20);
+        assert (indices.first == indices.second);
 
         System.out.println("Dutch national flag partition with pivot: " + -5);
         for (int i: inputs1) {
@@ -161,6 +282,22 @@ public class Sorting {
         }
         System.out.print("\n");
         System.out.println("[" + indices.first + "," + indices.second + ")");
+        validatePartition(inputs1, indices.first, indices.second, -5);
+        assert (indices.first == indices.second);
+
+        System.out.println("Dutch national flag partition with pivot: " + 7);
+        for (int i: inputs1) {
+            System.out.print(i + " ");
+        }
+        System.out.print("\n");
+        indices = dutchNationalFlagPartition(inputs1, 7);
+        for (int i: inputs1) {
+            System.out.print(i + " ");
+        }
+        System.out.print("\n");
+        System.out.println("[" + indices.first + "," + indices.second + ")");
+        validatePartition(inputs1, indices.first, indices.second, 7);
+        assert (indices.first == indices.second);
 
         System.out.println("Dutch national flag partition with pivot: " + 0);
         for (int i: inputs1) {
@@ -173,6 +310,7 @@ public class Sorting {
         }
         System.out.print("\n");
         System.out.println("[" + indices.first + "," + indices.second + ")");
+        validatePartition(inputs1, indices.first, indices.second, 0);
 
         System.out.println("Dutch national flag partition with pivot: " + 9);
         for (int i: inputs1) {
@@ -185,6 +323,7 @@ public class Sorting {
         }
         System.out.print("\n");
         System.out.println("[" + indices.first + "," + indices.second + ")");
+        validatePartition(inputs1, indices.first, indices.second, 9);
 
         Integer[] inputs2 = new Integer[] {4, 4, 4, 4, 4};
         System.out.println("Dutch national flag partition with pivot: " + 4);
@@ -198,6 +337,7 @@ public class Sorting {
         }
         System.out.print("\n");
         System.out.println("[" + indices.first + "," + indices.second + ")");
+        validatePartition(inputs2, indices.first, indices.second, 4);
 
         System.out.println("Dutch national flag partition with pivot: " + 2);
         for (int i: inputs2) {
@@ -210,6 +350,8 @@ public class Sorting {
         }
         System.out.print("\n");
         System.out.println("[" + indices.first + "," + indices.second + ")");
+        validatePartition(inputs2, indices.first, indices.second, 2);
+        assert (indices.first == indices.second);
 
         System.out.println("Dutch national flag partition with pivot: " + 8);
         for (int i: inputs2) {
@@ -222,6 +364,8 @@ public class Sorting {
         }
         System.out.print("\n");
         System.out.println("[" + indices.first + "," + indices.second + ")");
+        validatePartition(inputs2, indices.first, indices.second, 8);
+        assert (indices.first == indices.second);
 
         String[] inputs3 = new String[] {"red", "blue", "blue", "red", "white", "blue"};
         System.out.println("Dutch national flag partition with pivot: " + "blue");
@@ -234,6 +378,69 @@ public class Sorting {
             System.out.print(input + " ");
         }
         System.out.print("\n");
+        System.out.println("[" + indices.first + "," + indices.second + ")");
+        validatePartition(inputs3, indices.first, indices.second, "blue");
+
+        Integer[] inputs = new Integer[] {28, 58, 3, 89, 33, 89, 68, 80, 42, 10};
+        Integer[] testData = new Integer[inputs.length];
+
+        System.arraycopy(inputs, 0, testData, 0, inputs.length);
+        System.out.println("Dutch national flag partition with pivot: " + 33);
+        for (Integer input: testData) {
+            System.out.print(input + " ");
+        }
+        System.out.print("\n");
+        indices = dutchNationalFlagPartition_v2(testData, 33, 2, 7);
+        for (Integer input: testData) {
+            System.out.print(input + " ");
+        }
+        System.out.print("\n");
+        System.out.println("[" + indices.first + "," + indices.second + ")");
+        validatePartition(testData, 2, 7, indices.first, indices.second, 33);
+
+        System.arraycopy(inputs, 0, testData, 0, inputs.length);
+        System.out.println("Dutch national flag partition with pivot: " + 89);
+        for (Integer input: testData) {
+            System.out.print(input + " ");
+        }
+        System.out.print("\n");
+        indices = dutchNationalFlagPartition_v2(testData, 89, 2, 7);
+        for (Integer input: testData) {
+            System.out.print(input + " ");
+        }
+        System.out.print("\n");
+        System.out.println("[" + indices.first + "," + indices.second + ")");
+        validatePartition(testData, 2, 7, indices.first, indices.second, 89);
+
+        System.arraycopy(inputs, 0, testData, 0, inputs.length);
+        System.out.println("Dutch national flag partition with pivot: " + 58);
+        for (Integer input: testData) {
+            System.out.print(input + " ");
+        }
+        System.out.print("\n");
+        indices = dutchNationalFlagPartition_v2(testData, 58, 2, 7);
+        for (Integer input: testData) {
+            System.out.print(input + " ");
+        }
+        System.out.print("\n");
+        System.out.println("[" + indices.first + "," + indices.second + ")");
+        validatePartition(testData, 2, 7, indices.first, indices.second, 58);
+        assert (indices.first == indices.second);
+
+        System.arraycopy(inputs, 0, testData, 0, inputs.length);
+        System.out.println("Dutch national flag partition with pivot: " + 42);
+        for (Integer input: testData) {
+            System.out.print(input + " ");
+        }
+        System.out.print("\n");
+        indices = dutchNationalFlagPartition_v2(testData, 42, 2, 7);
+        for (Integer input: testData) {
+            System.out.print(input + " ");
+        }
+        System.out.print("\n");
+        System.out.println("[" + indices.first + "," + indices.second + ")");
+        validatePartition(testData, 2, 7, indices.first, indices.second, 42);
+        assert (indices.first == indices.second);
 
         System.out.println("quicksort");
         for (int i: inputs1) {
@@ -245,6 +452,7 @@ public class Sorting {
             System.out.print(i + " ");
         }
         System.out.print("\n");
+        validateSort(inputs1);
 
         System.out.println("quicksort");
         for (int i: inputs2) {
@@ -256,6 +464,7 @@ public class Sorting {
             System.out.print(i + " ");
         }
         System.out.print("\n");
+        validateSort(inputs2);
 
         System.out.println("quicksort");
         for (String input: inputs3) {
@@ -267,6 +476,7 @@ public class Sorting {
             System.out.print(input + " ");
         }
         System.out.print("\n");
+        validateSort(inputs3);
 
         Integer[] inputs4 = new Integer[] {1, 8, 5, 6, 2, 2, 7};
         System.out.println("quicksort");
@@ -279,6 +489,7 @@ public class Sorting {
             System.out.print(i + " ");
         }
         System.out.print("\n");
+        validateSort(inputs4);
 
         Integer[] inputs5 = new Integer[] {2, 2, 2, 8, 2, 4, 2};
         System.out.println("quicksort");
@@ -291,5 +502,6 @@ public class Sorting {
             System.out.print(i + " ");
         }
         System.out.print("\n");
+        validateSort(inputs5);
     }
 }
