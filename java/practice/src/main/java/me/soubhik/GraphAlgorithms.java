fb5a1a180feb50317d96d9b79a962bfb50097f6e
Soubhik <soubhik@gmail.com>
2018-10-02 22:22:00 +0530

    removed unused field: visited

--- java/practice/src/main/java/me/soubhik/GraphAlgorithms.java
+++ java/practice/src/main/java/me/soubhik/GraphAlgorithms.java
@@ -77,13 +77,11 @@ public class GraphAlgorithms {
         private Node node;
         private int distance;
         private int predecessor;
-        private boolean visited;
 
         public NodeInfo(Node node) {
             this.node = node;
             this.distance = Integer.MAX_VALUE;
             this.predecessor = -1;
-            this.visited = false;
         }
 
         public boolean update(int distance, int predecessor) {
@@ -94,10 +92,6 @@ public class GraphAlgorithms {
             }
 
             return false;
-        }
-
-        public void visit() {
-            this.visited = true;
         }
 
         @Override
